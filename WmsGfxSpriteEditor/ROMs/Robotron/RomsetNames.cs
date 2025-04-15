83b580b70f9766324b3cd4a686e810862e9577e7
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-04-15 16:31:38 +0100

    Add RomSetNames class for Robotron game labels
    Introduced a new static class `RomSetNames` in the namespace
    `WmsGfxSpriteEditor.ROMs.Robotron`. This class includes three
    constant string fields: `BlueLabel`, `TieDieWDPU`, and
    `TieDieMAME`, representing different labels for Robotron game
    variants.

---
+++ WmsGfxSpriteEditor/ROMs/Robotron/RomsetNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmsGfxSpriteEditor.ROMs.Robotron
+{
+    internal static class RomSetNames
+    {
+        public const string BlueLabel = "Robotron Blue Label";
+        public const string TieDieWDPU = "Robotron Tie Die (WDPU)";
+        public const string TieDieMAME = "Robotron Tie Die (WDPU)";
+    }
+}
