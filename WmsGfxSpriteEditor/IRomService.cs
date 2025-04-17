1b29ce4f21f4ddd529408cfd948b910ad27a4ae4
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-04-17 14:59:07 +0100

    Change LoadRomFiles return type to nullable MemoryStream

--- WmsGfxSpriteEditor/IRomService.cs
+++ WmsGfxSpriteEditor/IRomService.cs
@@ -15,8 +15,8 @@ namespace WmsGfxSpriteEditor
         /// Loads ROM files from the specified folder
         /// </summary>
         /// <param name="folderPath">Path to the folder containing ROM files</param>
-        /// <returns>MemoryStream containing the loaded ROM data</returns>
+        /// <returns>MemoryStream containing the loaded ROM data. null if ROMs could not be loaded.</returns>
         /// <exception cref="FileNotFoundException">Thrown when a required ROM file is missing</exception>
-        MemoryStream LoadRomFiles(string folderPath);
+        MemoryStream? LoadRomFiles(string folderPath);
     }
 }
