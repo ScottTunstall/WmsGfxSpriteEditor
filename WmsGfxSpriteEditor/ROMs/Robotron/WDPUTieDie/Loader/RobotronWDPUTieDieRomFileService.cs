edb3010faed73c3daf1afce15e52b7bc7d4e0b4d
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-04-17 14:57:34 +0100

    Using target typed new

--- WmsGfxSpriteEditor/ROMs/Robotron/WDPUTieDie/Loader/RobotronWDPUTieDieRomFileService.cs
+++ WmsGfxSpriteEditor/ROMs/Robotron/WDPUTieDie/Loader/RobotronWDPUTieDieRomFileService.cs
@@ -1,4 +1,4 @@
-﻿namespace WmsGfxSpriteEditor.ROMs.Robotron.WDPUTieDie.Loader
+namespace WmsGfxSpriteEditor.ROMs.Robotron.WDPUTieDie.Loader
 {
     /// <summary>
     /// Robotron Tie Die ROMS from Williams Defender Players Unite (WDPU)
@@ -7,18 +7,18 @@
     {
         protected override RomInfo[] RequiredRoms => new RomInfo[]
         {
-            new RomInfo("robotron.sb1", 0x0000, 0x1000),
-            new RomInfo("robotron.sb2", 0x1000, 0x1000),
-            new RomInfo("robotron.sb3", 0x2000, 0x1000),
-            new RomInfo("robotron.sb4", 0x3000, 0x1000),
-            new RomInfo("robotron.sb5", 0x4000, 0x1000),
-            new RomInfo("robotron.sb6", 0x5000, 0x1000),
-            new RomInfo("robotron.sb7", 0x6000, 0x1000),
-            new RomInfo("robotron.sb8", 0x7000, 0x1000),
-            new RomInfo("robotron.sb9", 0x8000, 0x1000),
-            new RomInfo("robotron.sba", 0xD000, 0x1000),
-            new RomInfo("robotron.sbb", 0xE000, 0x1000),
-            new RomInfo("robotron.sbc", 0xF000, 0x1000)
+            new("robotron.sb1", 0x0000, 0x1000),
+            new("robotron.sb2", 0x1000, 0x1000),
+            new("robotron.sb3", 0x2000, 0x1000),
+            new("robotron.sb4", 0x3000, 0x1000),
+            new("robotron.sb5", 0x4000, 0x1000),
+            new("robotron.sb6", 0x5000, 0x1000),
+            new("robotron.sb7", 0x6000, 0x1000),
+            new("robotron.sb8", 0x7000, 0x1000),
+            new("robotron.sb9", 0x8000, 0x1000),
+            new("robotron.sba", 0xD000, 0x1000),
+            new("robotron.sbb", 0xE000, 0x1000),
+            new("robotron.sbc", 0xF000, 0x1000)
         };
     }
 }
