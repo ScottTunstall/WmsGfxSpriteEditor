bd11bea30157515a350bdaf0dcb65ae865fc3c98
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-05-15 16:05:35 +0100

    Enhance history management and debugging features
    - Added debugging statements to track history index changes.
    - Modified the `Add` method to prevent duplicates in history.
    - Updated `HistoryItem` to use `required` properties for essential fields.
    - Removed `ZoomLevel` property to simplify `HistoryItem`.
    - Improved equality and hash code logic in `HistoryItem`.
    - Introduced `DumpHistory` method for better debugging output.

--- WmsGfxSpriteEditor/History/History.cs
+++ WmsGfxSpriteEditor/History/History.cs
@@ -1,9 +1,10 @@
+using System.Diagnostics;
+
 namespace WmsGfxSpriteEditor.History
 {
     public class History
     {
         private readonly List<HistoryItem> _historyItems = new();
-        
 
         /// <summary>
         /// Points to the current item in history
@@ -12,24 +13,21 @@ namespace WmsGfxSpriteEditor.History
 
         public void Add(HistoryItem item)
         {
+            // If Index is not equal to the last item in the list, the user has gone back in history with the undo function. 
+            // So, we need to replace *all* of the history starting from Index+1, with the new item
             if (Index < _historyItems.Count - 1)
             {
+                Debug.WriteLine("History index is {0}, removing items from {1} to {2}", Index, Index + 1, _historyItems.Count - 1);
                 // Remove all items after the current index
-                _historyItems.RemoveRange(Index, _historyItems.Count - Index - 1);
-            }
-
-            // Check if the item is the same as the last one added. We don't want duplicates in our history - pointless
-            if (Index > 0)
-            {
-                if (item.Equals(_historyItems[Index]))
-                {
-                    return;
-                }
+                _historyItems.RemoveRange(Index+1, _historyItems.Count - Index - 1);
             }
 
             _historyItems.Add(item);
-            Index++;
+            Index = _historyItems.Count-1;
+
+            DumpHistory();
         }
+
 
         public bool CanGoBack => Index > 0;
 
@@ -42,7 +40,9 @@ namespace WmsGfxSpriteEditor.History
                 return null;
             }
 
-            HistoryItem item = _historyItems[--Index];
+            --Index;
+            Debug.WriteLine("Back() - History index is now {0}", Index);
+            HistoryItem item = _historyItems[Index];
             return item;
         }
 
@@ -53,7 +53,9 @@ namespace WmsGfxSpriteEditor.History
                 return null;
             }
 
-            HistoryItem item = _historyItems[++Index];
+            Index++;
+            Debug.WriteLine("Forward() - History index is now {0}", Index);
+            HistoryItem item = _historyItems[Index];
             return item;
         }
 
@@ -61,6 +63,20 @@ namespace WmsGfxSpriteEditor.History
         {
             _historyItems.Clear();
             Index = -1;
+            DumpHistory();
+        }
+
+
+        [Conditional("DEBUG")]
+        private void DumpHistory()
+        {
+            Debug.WriteLine("History dump:");
+            Debug.WriteLine("Index: {0}", Index);
+            for (int i=0;i< _historyItems.Count; i++)
+            {
+                HistoryItem item = _historyItems[i];
+                Debug.WriteLine("Item {0}: {1}", i, item);
+            }
         }
     }
 }
--- WmsGfxSpriteEditor/History/HistoryItem.cs
+++ WmsGfxSpriteEditor/History/HistoryItem.cs
@@ -1,99 +1,48 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WmsGfxSpriteEditor.Sprites;
-
 namespace WmsGfxSpriteEditor.History
 {
     public enum OperationType
     {
         None = 0,
-        Zoom,
-        SpriteSelectionChanging,
-        SpriteDataChanging,
+        SelectedSpriteChanged,
+        BeforeSpritePixelDataChanged,
+        AfterSpritePixelDataChanged,
     }
-
 
     public record HistoryItem
     {
-        public OperationType OperationType { get; set; }
-        public int SpriteIndex { get; set; }
-        public int ZoomLevel { get; set; }
+        public required OperationType OperationType { get; init; }
+        public required int SpriteIndex { get; init; }
         public byte[]? PixelData { get; set; }
+        public UInt128? PixelDataHash { get; set; }
 
-
-        // Override Equals to handle byte[] comparison
-        public virtual bool Equals(HistoryItem? other)
-        {
-            if (other is null) return false;
-
-            if (OperationType != other.OperationType ||
-                SpriteIndex != other.SpriteIndex ||
-                ZoomLevel != other.ZoomLevel)
-                return false;
-
-            // Are both PixelData set to null. Then compare succeeds
-            if (PixelData == null && other.PixelData == null)
-                return true;
-
-            // One of the PixelData is null, but not both
-            if (PixelData == null || other.PixelData == null)
-                return false;
-            
-            // Compare array contents
-            return PixelData!.SequenceEqual(other.PixelData);
-        }
-
-        // Override GetHashCode to be consistent with Equals
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hash = 17;
-                hash = hash * 23 + OperationType.GetHashCode();
-                hash = hash * 23 + SpriteIndex.GetHashCode();
-                hash = hash * 23 + ZoomLevel.GetHashCode();
-
-                // Include SpriteData in hash calculation if it exists
-                if (PixelData != null)
-                {
-                    foreach (var b in PixelData)
-                    {
-                        hash = hash * 23 + b.GetHashCode();
-                    }
-                }
-
-                return hash;
-            }
-        }
-
-
-        public static HistoryItem CreateZoomHistoryItem( int zoomLevel)
+        public static HistoryItem CreateSpriteSelectionChangedHistoryItem(int selectedSpriteIndex)
         {
             return new HistoryItem()
             {
-                OperationType = OperationType.Zoom,
-                ZoomLevel = zoomLevel
-            };
-        }
-        public static HistoryItem CreateSpriteSelectionChangingHistoryItem(int selectedSpriteIndex)
-        {
-            return new HistoryItem()
-            {
-                OperationType = OperationType.SpriteSelectionChanging,
+                OperationType = OperationType.SelectedSpriteChanged,
                 SpriteIndex = selectedSpriteIndex
             };
         }
 
-        public static HistoryItem CreateSpriteDataChangingHistoryItem(ISprite sprite, int selectedSpriteIndex, int spriteOffset)
+        public static HistoryItem CreateBeforeSpritePixelDataChangedHistoryItem(ISprite sprite, int selectedSpriteIndex)
         {
             return new HistoryItem()
             {
-                OperationType = OperationType.SpriteDataChanging,
+                OperationType = OperationType.BeforeSpritePixelDataChanged,
                 SpriteIndex = selectedSpriteIndex,
                 PixelData = sprite.ClonePixelData(),
+                PixelDataHash = sprite.GetPixelDataHash(),
+            };
+        }
+
+        public static HistoryItem CreateAfterSpritePixelDataChangedHistoryItem(ISprite sprite, int selectedSpriteIndex)
+        {
+            return new HistoryItem()
+            {
+                OperationType = OperationType.AfterSpritePixelDataChanged,
+                SpriteIndex = selectedSpriteIndex,
+                PixelData = sprite.ClonePixelData(),
+                PixelDataHash = sprite.GetPixelDataHash()
             };
         }
     }
