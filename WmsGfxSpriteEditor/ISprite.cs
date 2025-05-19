9bc6e631aec072f26c882cb8df6da32f85e03454
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-05-19 19:58:21 +0100

    Added IsInBounds method to ISprite

--- WmsGfxSpriteEditor/ISprite.cs
+++ WmsGfxSpriteEditor/ISprite.cs
@@ -4,6 +4,7 @@ namespace WmsGfxSpriteEditor
     {
         // We use Memory<byte> so that we can share memory between the Sprite being edited and the MemoryStream it originates from
         Memory<byte> PixelData { get; set; }
+
         int Width { get; set; }
         int WidthInBytes { get; set; }
         int Height { get; set; }
@@ -13,13 +14,14 @@ namespace WmsGfxSpriteEditor
 
         void ClearPixelDataDirtyFlag();
 
+        bool IsInBounds(int x, int y);
+
         int GetPaletteIndexFromPixel(int x, int y);
+
         void SetPixelByPaletteIndex(int x, int y, int paletteIndex);
 
         byte[] ClonePixelData();
 
         UInt128 GetPixelDataHash();
-
     }
-
 }
