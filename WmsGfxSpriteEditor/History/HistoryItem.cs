95c841b6be38a9ce10031937bb4bbfaf7bae567c
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-05-23 14:41:02 +0100

    Remove sprite selection history methods and update enums
    - Removed `SelectedSpriteChanged` from `OperationType`.
    - Deleted `CreateSpriteSelectionChangedHistoryItem` method.
    - Updated `CreateBeforeSpritePixelDataChangedHistoryItem` to use `byte[]` and `UInt128` parameters.
    - Updated `CreateAfterSpritePixelDataChangedHistoryItem` to use `byte[]` and `UInt128` parameters.

--- WmsGfxSpriteEditor/History/HistoryItem.cs
+++ WmsGfxSpriteEditor/History/HistoryItem.cs
@@ -3,7 +3,6 @@ namespace WmsGfxSpriteEditor.History
     public enum OperationType
     {
         None = 0,
-        SelectedSpriteChanged,
         BeforeSpritePixelDataChanged,
         AfterSpritePixelDataChanged,
     }
@@ -15,34 +14,27 @@ namespace WmsGfxSpriteEditor.History
         public byte[]? PixelData { get; set; }
         public UInt128? PixelDataHash { get; set; }
 
-        public static HistoryItem CreateSpriteSelectionChangedHistoryItem(int selectedSpriteIndex)
-        {
-            return new HistoryItem()
-            {
-                OperationType = OperationType.SelectedSpriteChanged,
-                SpriteIndex = selectedSpriteIndex
-            };
-        }
 
-        public static HistoryItem CreateBeforeSpritePixelDataChangedHistoryItem(ISprite sprite, int selectedSpriteIndex)
+
+        public static HistoryItem CreateBeforeSpritePixelDataChangedHistoryItem(byte[] pixelDataBeforeChangeMade, UInt128 hash, int selectedSpriteIndex)
         {
             return new HistoryItem()
             {
                 OperationType = OperationType.BeforeSpritePixelDataChanged,
                 SpriteIndex = selectedSpriteIndex,
-                PixelData = sprite.ClonePixelData(),
-                PixelDataHash = sprite.GetPixelDataHash(),
+                PixelData = pixelDataBeforeChangeMade,
+                PixelDataHash = hash,
             };
         }
 
-        public static HistoryItem CreateAfterSpritePixelDataChangedHistoryItem(ISprite sprite, int selectedSpriteIndex)
+        public static HistoryItem CreateAfterSpritePixelDataChangedHistoryItem(byte[] pixelDataAfterChangeMade, UInt128 hash, int selectedSpriteIndex)
         {
             return new HistoryItem()
             {
                 OperationType = OperationType.AfterSpritePixelDataChanged,
                 SpriteIndex = selectedSpriteIndex,
-                PixelData = sprite.ClonePixelData(),
-                PixelDataHash = sprite.GetPixelDataHash()
+                PixelData = pixelDataAfterChangeMade,
+                PixelDataHash = hash
             };
         }
     }
