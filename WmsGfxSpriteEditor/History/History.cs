f4c807d63eb4e4e6609fb82428be3215c1f29754
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-05-29 14:50:27 +0100

    Improve code formatting and readability
    - Change predicate type in `Last` method to `Predicate<HistoryItem>`.

--- WmsGfxSpriteEditor/History/History.cs
+++ WmsGfxSpriteEditor/History/History.cs
@@ -15,21 +15,20 @@ namespace WmsGfxSpriteEditor.History
 
         public void Add(HistoryItem item)
         {
-            // If Index is not equal to the last item in the list, the user has gone back in history with the undo function. 
+            // If Index is not equal to the last item in the list, the user has gone back in history with the undo function.
             // So, we need to replace *all* of the history starting from Index+1, with the new item
             if (Index < _historyItems.Count - 1)
             {
                 Debug.WriteLine("History index is {0}, removing items from {1} to {2}", Index, Index + 1, _historyItems.Count - 1);
                 // Remove all items after the current index
-                _historyItems.RemoveRange(Index+1, _historyItems.Count - Index - 1);
+                _historyItems.RemoveRange(Index + 1, _historyItems.Count - Index - 1);
             }
 
             _historyItems.Add(item);
-            Index = _historyItems.Count-1;
+            Index = _historyItems.Count - 1;
 
             DumpHistory();
         }
-
 
         /// <summary>
         /// Returns the last <see cref="HistoryItem"/> in the history that matches the specified predicate, searching backward from a given start index.
@@ -38,11 +37,11 @@ namespace WmsGfxSpriteEditor.History
         /// <param name="startIndex">The index to start searching backward from. If less than 0, starts from the item before the current <see cref="Index"/>.</param>
         /// <returns>The last <see cref="HistoryItem"/> that matches the predicate, or <c>null</c> if no match is found.</returns>
         /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="startIndex"/> is 0 or greater than or equal to the number of history items.</exception>
-        public HistoryItem? Last(Func<HistoryItem, bool> predicate, int startIndex = -1)
+        public HistoryItem? Last(Predicate<HistoryItem> predicate, int startIndex = -1)
         {
-            if (startIndex <0)
+            if (startIndex < 0)
             {
-                startIndex = Index; // Start from the last added item 
+                startIndex = Index; // Start from the last added item
             }
 
             if (startIndex >= _historyItems.Count)
@@ -61,10 +60,8 @@ namespace WmsGfxSpriteEditor.History
                 }
             }
 
-
             return null;
         }
-
 
         public bool CanGoBack => Index > 0;
 
@@ -86,7 +83,7 @@ namespace WmsGfxSpriteEditor.History
 
         public HistoryItem? Forward()
         {
-            if (Index >= _historyItems.Count-1)
+            if (Index >= _historyItems.Count - 1)
             {
                 throw new InvalidOperationException($"Cannot go forward, {nameof(Index)} is at the end of the history.");
             }
@@ -105,14 +102,12 @@ namespace WmsGfxSpriteEditor.History
             DumpHistory();
         }
 
-
-
         [Conditional("DEBUG")]
         private void DumpHistory()
         {
             Debug.WriteLine("History dump:");
             Debug.WriteLine("Index: {0}", Index);
-            for (int i=0;i< _historyItems.Count; i++)
+            for (int i = 0; i < _historyItems.Count; i++)
             {
                 HistoryItem item = _historyItems[i];
                 Debug.WriteLine("Item {0}: {1}", i, item);
