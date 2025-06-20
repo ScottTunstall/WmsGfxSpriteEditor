9b86772fe3fe29bdadaa8be1073fa8acac73ad91
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-06-20 12:26:23 +0100

    Add history management system to WmsGfxSpriteEditor
    - Implemented History class to manage history items.
    - Created HistoryItem record for tracking sprite changes.
    - Defined IHistory interface for history management contract.
    - Updated project file to target .NET 8.0 and enable features.

---
+++ WmsGfxSpriteEditor.History/HistoryItem.cs
@@ -0,0 +1,27 @@
+namespace WmsGfxSpriteEditor.History
+{
+    public enum OperationType
+    {
+        None = 0,
+        SpritePixelDataSnapshot
+    }
+
+    public record HistoryItem
+    {
+        public required OperationType OperationType { get; init; }
+        public required int SpriteIndex { get; init; }
+        public byte[]? PixelData { get; set; }
+        public UInt128? PixelDataHash { get; set; }
+        
+        public static HistoryItem CreateSpritePixelDataChangedHistoryItem(byte[] pixelDataBeforeChangeMade, UInt128 hash, int spriteIndex)
+        {
+            return new HistoryItem()
+            {
+                OperationType = OperationType.SpritePixelDataSnapshot,
+                SpriteIndex= spriteIndex,
+                PixelData = pixelDataBeforeChangeMade,
+                PixelDataHash = hash,
+            };
+        }
+    }
+}
