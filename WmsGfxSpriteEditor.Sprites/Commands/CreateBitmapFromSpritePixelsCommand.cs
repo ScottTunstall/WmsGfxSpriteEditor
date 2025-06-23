504c28bf12d8d4a826f53355e94e4bdd19926c33
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-06-23 13:12:37 +0100

    Add sprite bitmap command classes
    - Add CreateBitmapFromSpritePixelsCommand class
    - Add SetSpritePixelsFromBitmapCommand class
    - Implement Execute method in CreateBitmapFromSpritePixelsCommand
    - Implement Execute method in SetSpritePixelsFromBitmapCommand with undo/redo support

---
+++ WmsGfxSpriteEditor.Sprites/Commands/CreateBitmapFromSpritePixelsCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace WmsGfxSpriteEditor.Sprites.Commands
+{
+    public class CreateBitmapFromSpritePixelsCommand
+    {
+        public Bitmap Execute(ISprite source)
+        {
+            return source.CreateBitmapFromSprite();
+        }
+    }
+}
---
+++ WmsGfxSpriteEditor.Sprites/Commands/SetSpritePixelsFromBitmapCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace WmsGfxSpriteEditor.Sprites.Commands;
+
+public class SetSpritePixelsFromBitmapCommand
+{
+    private readonly UndoableSpriteHelper _undoHelper;
+
+    public SetSpritePixelsFromBitmapCommand(IHistory history)
+    {
+        _undoHelper = new UndoableSpriteHelper(history);
+    }
+
+    public void Execute(Bitmap source, ISprite target)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        
+        _undoHelper.ExecuteActionWithUndoRedo(target, () =>
+        {
+            for (int y = 0; y < source.Height; y++)
+            {
+                for (int x = 0; x < source.Width; x++)
+                {
+                    Color pixelColor = source.GetPixel(x, y);
+                    int paletteIndex = Array.IndexOf(target.Palette, pixelColor);
+                    target.SetPixelByPaletteIndex(x,y, paletteIndex);
+                }
+            }
+        } );
+    }
+}
+    
+
