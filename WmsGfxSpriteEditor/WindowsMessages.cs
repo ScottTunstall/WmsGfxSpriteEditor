3521381ae7f337b6f43d95745018374073a0d187
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-08 11:50:32 +0100

    Add WindowsMessages class in WmsGfxSpriteEditor namespace
    - Introduced a new namespace `WmsGfxSpriteEditor`.
    - Added class `WindowsMessages` with constant `WM_CLIPBOARDUPDATE`.
    - Included pragmas to suppress warnings for underscores in identifiers.

---
+++ WmsGfxSpriteEditor/WindowsMessages.cs
@@ -0,0 +1,9 @@
+namespace WmsGfxSpriteEditor
+{
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+    public class WindowsMessages
+    {
+        public const int WM_CLIPBOARDUPDATE = 0x031D;
+    }
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+}
