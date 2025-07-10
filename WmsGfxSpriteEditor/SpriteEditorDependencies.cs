b8c7c41379a2feee52b779e390835afcbedf285c
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-10 14:08:32 +0100

    Enhance SpriteEditorDependencies for ROM services
    - Added `IRomService` parameter to `SpriteEditorDependencies`.
    - Renamed factory method to `CreateForRobotron` for clarity.
    - Updated switch statement to handle `RobotronRomSetType` values.
    - Added new cases for `TieDieMAME` in the factory method.

--- WmsGfxSpriteEditor/SpriteEditorDependencies.cs
+++ WmsGfxSpriteEditor/SpriteEditorDependencies.cs
@@ -1,9 +1,10 @@
 using WmsGfxSpriteEditor.Palettes;
+using WmsGfxSpriteEditor.Roms;
 using WmsGfxSpriteEditor.Sprites;
 
 namespace WmsGfxSpriteEditor
 {
-    public record SpriteEditorDependencies(IPaletteService PaletteService, ISpriteRepository SpriteRepository, ISpriteFactory SpriteFactory, ISpriteGridRenderer SpriteRenderer)
+    public record SpriteEditorDependencies(IRomService RomService, IPaletteService PaletteService, ISpriteRepository SpriteRepository, ISpriteFactory SpriteFactory, ISpriteGridRenderer SpriteRenderer)
     {
     }
 }
--- WmsGfxSpriteEditor/SpriteEditorDependenciesFactory.cs
+++ WmsGfxSpriteEditor/SpriteEditorDependenciesFactory.cs
@@ -1,18 +1,22 @@
 using WmsGfxSpriteEditor.Roms.Robotron2084;
-using WmsGfxSpriteEditor.Roms.Robotron2084.Shared;
+using WmsGfxSpriteEditor.Roms.Robotron2084.BlueLabel;
+using WmsGfxSpriteEditor.Roms.Robotron2084.MAMETieDie;
 using WmsGfxSpriteEditor.Roms.Robotron2084.Shared.Palettes;
+using WmsGfxSpriteEditor.Roms.Robotron2084.Shared.Sprites;
+using WmsGfxSpriteEditor.Roms.Robotron2084.WDPUTieDie;
 using WmsGfxSpriteEditor.Sprites;
 
 namespace WmsGfxSpriteEditor
 {
     public class SpriteEditorDependenciesFactory
     {
-        public static SpriteEditorDependencies Create(RomSetType romSetTypes)
+        public static SpriteEditorDependencies CreateForRobotron(RobotronRomSetType romSetTypes)
         {
             return romSetTypes switch
             {
-                RomSetType.BlueLabel => new SpriteEditorDependencies( new RobotronPaletteService(), new RobotronBlueLabelSpriteRepository(), new SpriteFactory(), new DefaultSpriteGridRenderer()),
-                RomSetType.TieDieWDPU => new SpriteEditorDependencies( new RobotronPaletteService(), new RobotronBlueLabelSpriteRepository(), new SpriteFactory(), new DefaultSpriteGridRenderer()),
+                RobotronRomSetType.BlueLabel => new SpriteEditorDependencies(new RobotronBlueLabelRomService(), new RobotronPaletteService(), new RobotronBlueLabelSpriteRepository(), new RobotronSpriteFactory(), new DefaultSpriteGridRenderer()),
+                RobotronRomSetType.TieDieWDPU => new SpriteEditorDependencies( new RobotronWDPUTieDieRomService(), new RobotronPaletteService(), new RobotronBlueLabelSpriteRepository(), new RobotronSpriteFactory(), new DefaultSpriteGridRenderer()),
+                RobotronRomSetType.TieDieMAME => new SpriteEditorDependencies( new RobotronMAMETieDieRomService(), new RobotronPaletteService(), new RobotronBlueLabelSpriteRepository(), new RobotronSpriteFactory(), new DefaultSpriteGridRenderer()),
                 _ => throw new NotSupportedException($"Robotron ROM set type {romSetTypes} is not supported.")
             };
         }
