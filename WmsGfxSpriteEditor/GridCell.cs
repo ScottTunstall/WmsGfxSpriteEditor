dbad1ebba422042224f7325e6f57a174c041d89f
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-16 10:23:17 +0100

    Added stylecop suppressions for record types

--- WmsGfxSpriteEditor/GridCell.cs
+++ WmsGfxSpriteEditor/GridCell.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace WmsGfxSpriteEditor
 {
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
     public record struct GridCell (int X, int Y)
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
     {
     }
 }
--- WmsGfxSpriteEditor/SpriteEditorDependencies.cs
+++ WmsGfxSpriteEditor/SpriteEditorDependencies.cs
@@ -4,7 +4,9 @@ using WmsGfxSpriteEditor.Sprites;
 
 namespace WmsGfxSpriteEditor
 {
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
     public record SpriteEditorDependencies(IRomService RomService, IPaletteService PaletteService, ISpriteRepository SpriteRepository, ISpriteFactory SpriteFactory, ISpriteGridRenderer SpriteRenderer)
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
     {
     }
 }
