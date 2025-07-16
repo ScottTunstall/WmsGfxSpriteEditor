c77440ee8c0825e5aed6812a12ad6ba4b3861cce
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-16 13:07:10 +0100

    Add AboutDialog class for application information display
    - Introduced `AboutDialog` class in `WmsGfxSpriteEditor.Dialogs` namespace.
    - Implemented `ShowDialog` method to show application details in a message box.

---
+++ WmsGfxSpriteEditor/Dialogs/AboutDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmsGfxSpriteEditor.Dialogs
+{
+    internal class AboutDialog
+    {
+        public void ShowDialog(IWin32Window? owner = null)
+        {
+            MessageBox.Show(
+                owner,
+                "Williams Graphics Sprite Editor." + Environment.NewLine +
+                Environment.NewLine +
+                "Designed and developed by Scott Tunstall." + Environment.NewLine +
+                "Sprite offsets discovered and documented by Sean Riddle." + Environment.NewLine +
+                "All rights reserved.",
+                "About Williams Graphics Sprite Editor",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.None
+            );
+        }
+    }
+}
