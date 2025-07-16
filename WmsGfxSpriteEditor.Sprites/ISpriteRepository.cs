c808941061fc570cb553a98a16c6da1b0cef10eb
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-16 22:55:31 +0100

    Added xmldocs to ISprite and ISpriteRepository

--- WmsGfxSpriteEditor.Sprites/ISpriteRepository.cs
+++ WmsGfxSpriteEditor.Sprites/ISpriteRepository.cs
@@ -1,26 +1,26 @@
 namespace WmsGfxSpriteEditor.Sprites
 {
     /// <summary>
-    /// Defines a repository for accessing sprite information
+    /// Defines a repository for accessing and retrieving sprite information.
     /// </summary>
     public interface ISpriteRepository
     {
         /// <summary>
-        /// Gets all available sprites
+        /// Gets the number of sprites in the repository.
         /// </summary>
-        /// <returns>A collection of sprite information</returns>
+        int Count { get; }
+
+        /// <summary>
+        /// Gets all available sprites in the repository.
+        /// </summary>
+        /// <returns>A collection of sprite information.</returns>
         IReadOnlyCollection<SpriteInfo> GetAllSprites();
 
         /// <summary>
-        /// Gets a sprite by its index
+        /// Gets a sprite by its index.
         /// </summary>
-        /// <param name="index">The zero-based index of the sprite</param>
-        /// <returns>The sprite information or null if the index is out of range</returns>
+        /// <param name="index">The zero-based index of the sprite.</param>
+        /// <returns>The sprite information or null if the index is out of range.</returns>
         SpriteInfo? GetSpriteInfoByIndex(int index);
-
-        /// <summary>
-        /// Gets the number of sprites in the repository
-        /// </summary>
-        int Count { get; }
     }
 }
