780c7ae2174fe5fac4476ae9a5649c0637e2b144
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-21 15:55:29 +0100

    Enhance CreateCrosshair method in CrosshairCursor
    - Added outlineColour parameter to CreateCrosshair method.
    - Changed default size from 16 to 12.
    - Implemented drawing logic for an outline around the crosshair.
    - Updated vertical and horizontal line drawing to use center calculation.

--- WmsGfxSpriteEditor/Cursors/CrosshairCursor.cs
+++ WmsGfxSpriteEditor/Cursors/CrosshairCursor.cs
@@ -2,16 +2,32 @@ namespace WmsGfxSpriteEditor.Cursors
 {
     internal class CrosshairCursor
     {
-        public static Cursor CreateCrosshair(Color colour, int size = 16)
+        public static Cursor CreateCrosshair(Color colour, Color outlineColour, int size = 12)
         {
             Bitmap bmp = new Bitmap(size, size);
             using (Graphics g = Graphics.FromImage(bmp))
             {
                 g.Clear(Color.Transparent);
+                
+                int center = size / 2;
+                
+                // Draw outline first - creates a contiguous border around the crosshair
+                using (Pen outlinePen = new Pen(outlineColour, 1))
+                {
+                    // Vertical white outline lines (left and right of the center vertical line)
+                    g.DrawLine(outlinePen, center - 1, 0, center - 1, size - 1);     // left side
+                    g.DrawLine(outlinePen, center + 1, 0, center + 1, size - 1);     // right side
+                    
+                    // Horizontal white outline lines (above and below the center horizontal line)
+                    g.DrawLine(outlinePen, 0, center - 1, size - 1, center - 1);     // top side
+                    g.DrawLine(outlinePen, 0, center + 1, size - 1, center + 1);     // bottom side
+                }
+                
+                // Draw the main crosshair (using the provided colour parameter)
                 using (Pen pen = new Pen(colour, 1))
                 {
-                    g.DrawLine(pen, size / 2, 0, size / 2, size); // vertical line
-                    g.DrawLine(pen, 0, size / 2, size, size / 2); // horizontal line
+                    g.DrawLine(pen, center, 0, center, size - 1);         // vertical line
+                    g.DrawLine(pen, 0, center, size - 1, center);         // horizontal line
                 }
             }
             return new Cursor(bmp.GetHicon());
