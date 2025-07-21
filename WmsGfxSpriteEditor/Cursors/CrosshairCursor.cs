34cf3b7be63c254e4701b8f0302f4621a5622b9c
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-21 16:46:37 +0100

    Stylecop whining

--- WmsGfxSpriteEditor/Cursors/CrosshairCursor.cs
+++ WmsGfxSpriteEditor/Cursors/CrosshairCursor.cs
@@ -9,21 +9,21 @@ namespace WmsGfxSpriteEditor.Cursors
             using (Graphics g = Graphics.FromImage(bmp))
             {
                 g.Clear(Color.Transparent);
-                
+
                 int center = size / 2;
-                
+
                 // Draw outline first - creates a contiguous border around the crosshair
                 using (Pen outlinePen = new Pen(outlineColour, 1))
                 {
                     // Vertical white outline lines (left and right of the center vertical line)
                     g.DrawLine(outlinePen, center - 1, 0, center - 1, size - 1);     // left side
                     g.DrawLine(outlinePen, center + 1, 0, center + 1, size - 1);     // right side
-                    
+
                     // Horizontal white outline lines (above and below the center horizontal line)
                     g.DrawLine(outlinePen, 0, center - 1, size - 1, center - 1);     // top side
                     g.DrawLine(outlinePen, 0, center + 1, size - 1, center + 1);     // bottom side
                 }
-                
+
                 // Draw the main crosshair (using the provided colour parameter)
                 using (Pen pen = new Pen(colour, 1))
                 {
