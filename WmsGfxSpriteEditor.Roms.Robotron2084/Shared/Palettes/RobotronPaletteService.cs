6c91122bfbc5e53aa158ae0789ce51f0cbd52fa4
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-21 14:54:15 +0100

    Enhance RobotronPaletteService.cs comments and logic
    - Added credits and reference for color conversion algorithm.
    - Modified color values array to prevent duplicates affecting clipboard functionality.
    - Clarified comments in ConvertColorValue method for RGB extraction.

--- WmsGfxSpriteEditor.Roms.Robotron2084/Shared/Palettes/RobotronPaletteService.cs
+++ WmsGfxSpriteEditor.Roms.Robotron2084/Shared/Palettes/RobotronPaletteService.cs
@@ -1,4 +1,5 @@
 // Credit to Sean Riddle for designing the algorithm which converts byte colour values to their RGB equivalent
+// See: https://www.seanriddle.com/ripper.html
 
 using System.Drawing;
 using WmsGfxSpriteEditor.Palettes;
@@ -7,15 +8,29 @@ namespace WmsGfxSpriteEditor.Roms.Robotron2084.Shared.Palettes
 {
     /// <summary>
     /// Provides the color palette from the Robotron arcade game
+    /// Colours are stored as a byte, in BBGGGRRR format
     /// </summary>
     public class RobotronPaletteService : IPaletteService
     {
-        // Original color values from Robotron ROM
+        // Original color values from Robotron ROM - as supplied by Sean Riddle
+        // and present in my disassembly at $DA51.
+
+        //private static readonly byte[] _colorValues =
+        //[
+        //    0x00, 0x07, 0x17, 0xc7, 0x1f, 0x3f, 0x38, 0xc0,
+        //    0xa4, 0xff, 0x38, 0x17, 0xcc, 0x81, 0x81, 0x07
+        //];
+
+        // Modified to prevent duplicates, which would break the clipboard|paste functionality in the sprite editor.
+        // Colour index 10 (zero based index) onwards are used for colour cycling in game.
         private static readonly byte[] _colorValues =
         [
             0x00, 0x07, 0x17, 0xc7, 0x1f, 0x3f, 0x38, 0xc0,
-            0xa4, 0xff, 0x38, 0x17, 0xcc, 0x81, 0x81, 0x07
+            0xa4, 0xff,
+            // Cycling colours
+            0xc4, 0xf4, 0xcc, 0x81, 0x45, 0x2f
         ];
+
 
         // The actual RGB colors derived from the hardware values
         private readonly Color[] _palette;
@@ -43,18 +58,21 @@ namespace WmsGfxSpriteEditor.Roms.Robotron2084.Shared.Palettes
         // This code which converts bytes to RGB was ported from Sean Riddle's Williams Graphics Ripper
         private static Color ConvertColorValue(byte value)
         {
+            // The red component is stored in bits 0-2 
             int red = (value & 0x7) << 1;
             if (red > 6)
             {
                 red++;
             }
 
+            // The green component is stored in bits 3-5 
             int green = (value & 0x38) >> 2;
             if (green > 6)
             {
                 green++;
             }
 
+            // The blue component is stored in the top 2 bits 
             int blue = ((value & 0xc0) >> 6) * 5;
 
             // Ensure values are in valid range
