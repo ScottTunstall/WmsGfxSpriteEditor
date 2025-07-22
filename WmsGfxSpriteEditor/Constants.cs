fba60d445a772fdffffcefaed252567f9a97490e
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-22 13:59:22 +0100

    Add Constants class for application strings
    - Introduced `Constants` class in `WmsGfxSpriteEditor` namespace
    - Added public constant strings: `AppTitle` and `NoRomLoaded`

---
+++ WmsGfxSpriteEditor/Constants.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmsGfxSpriteEditor
+{
+    internal class Constants
+    {
+        public const string AppTitle = "Sprite Editor";
+        public const string NoRomLoaded = "No ROMset loaded";
+    }
+}
