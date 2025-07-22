026d3e4ba36b9b6073e158ddc4e6618343c141e1
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-22 13:59:58 +0100

    Refactor Constants in Constants.cs
    - Renamed constant `NoRomLoaded` to `NoRomsetLoaded`

--- WmsGfxSpriteEditor/Constants.cs
+++ WmsGfxSpriteEditor/Constants.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace WmsGfxSpriteEditor
 {
     internal class Constants
     {
         public const string AppTitle = "Sprite Editor";
-        public const string NoRomLoaded = "No ROMset loaded";
+        public const string NoRomsetLoaded = "No ROMset loaded";
     }
 }
