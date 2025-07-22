88833e4dc26bca1154ca04f77d2b86e1742b162a
Scott Tunstall <scott.tunstall@ntlworld.com>
2025-07-22 14:30:05 +0100

    Add HelpUrl constant to Constants class
    - Introduced a new constant `HelpUrl` in `Constants.cs`
    - Provides a URL to the GitHub repository for WmsGfxSpriteEditor

--- WmsGfxSpriteEditor/Constants.cs
+++ WmsGfxSpriteEditor/Constants.cs
@@ -4,5 +4,6 @@ namespace WmsGfxSpriteEditor
     {
         public const string AppTitle = "Sprite Editor";
         public const string NoRomsetLoaded = "No ROMset loaded";
+        public const string HelpUrl = "https://github.com/ScottTunstall/WmsGfxSpriteEditor";
     }
 }
